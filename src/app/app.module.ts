a027ccaf4769f4d1fa031fece697737310615fd4
amsakanna <amsakanna@gmail.com>
2017-03-12 20:10:16 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA3dca3bAlT4gR6pytsEZDOJVKFRU3ptE4",
+    authDomain: "shopping-81d9a.firebaseapp.com",
+    databaseURL: "https://shopping-81d9a.firebaseio.com",
+    storageBucket: "shopping-81d9a.appspot.com",
+    messagingSenderId: "1011127812612"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
